4c3dec610f8cc40337c682697f2ffbb12bcdb8d6
Jose Rafael Guaura <jguaura@gmail.com>
2023-02-27 17:43:57 -0500

    refactor:
    - retrieving query params and navigation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,15 +7,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
-  
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onLoadServers() {
+  onLoadServers(id: number) {
     // - xdXdxD
-    this.router.navigate(['/servers'])
+    this.router.navigate(['/servers', id, 'edit'], {
+      queryParams: { allowEdit: '1' },
+      fragment: 'edit'
+    });
   }
-
 }
--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
@@ -12,9 +13,11 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
