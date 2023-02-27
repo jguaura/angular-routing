8115cb1cd9a3194a053d82b8350af624d817306c
Jose Rafael Guaura <jguaura@gmail.com>
2023-02-27 13:31:58 -0500

    feat:
    - fetching route params reactively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -16,6 +16,13 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      )
   }
 
 }
